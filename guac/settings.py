bf343ffe4a5dbf167d003cab306b6f8ebf0a9a07
Dan Jelf <dkjelf@email.wm.edu>
2016-07-14 13:20:15 -0400

    first

--- guac/settings.py
+++ guac/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#dn=9w=1g-uy1c(1xuem7$#giw$oad2b+x5su5^o8^uiw=la2o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
